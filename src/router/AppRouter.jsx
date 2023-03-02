594fd34d2b246807f632f7bfc2f3e6254a21f957
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-02 10:54:26 -0600

    navbar y archivos de barril

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { LoginPage } from '../auth/pages/LoginPage'
-import { DCPage } from '../heroes/pages/DCPage'
-import { MarverPage } from '../heroes/pages/MarverPage'
+import { LoginPage } from '../auth'
+import { MarverPage, DCPage } from '../heroes'
+import { Navbar } from '../ui'
 
 export const AppRouter = () => {
     return (
         <>
+            <Navbar />
             <Routes>
-                <Route path='/marvel' element={<MarverPage/>} />
+                <Route path='/marvel' element={<MarverPage />} />
                 <Route path='/dc' element={<DCPage />} />
                 <Route path='/login' element={<LoginPage />} />
                 <Route path='/' element={<Navigate to="/login" />} />
